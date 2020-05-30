4f9dbe1155d63e46af074fb09823753f76218085
marcofaggian <m@marcofaggian.com>
2020-05-30 12:51:30 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import rules from "./rules";
+import { JsonEditor as Editor } from "jsoneditor-react";
+import "jsoneditor-react/es/editor.min.css";
+import useClippy from "use-clippy";
+
+const App = () => {
+  const [statedRules, setStatedRules] = useState(rules);
+  const [clipboard, setClipboard] = useClippy();
+  let [input, setInput] = useState("");
+
+  let computed = (input || "Enter your text here...").replace(
+    new RegExp(Object.keys(statedRules).join("|"), "gi"),
+    (matched: string) => (statedRules as any)[matched.toLowerCase()]
+  );
+  let isInClipboard = clipboard === computed;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ marginTop: "20vh" }}>H4ck3rC4s3_1t!</h1>
+      <h1
+        style={{
+          marginTop: "-32px",
+          zIndex: -1,
+          color: "rgba(0,0,0,0.3)",
+          fontSize: "22px",
+        }}
+      >
+        HackerCase It!
+      </h1>
+
+      <h3 style={{ marginBottom: "4px" }}>You can change the rules</h3>
+      <div style={{ minWidth: "328px" }}>
+        <Editor
+          value={statedRules}
+          onChange={(v: any) => {
+            console.log("ciao", v);
+            setStatedRules(v);
+          }}
+        />
+      </div>
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter your text here..."
+        style={{ marginTop: "24px", minWidth: "320px", minHeight: "32px" }}
+      />
+      <div style={{ marginTop: "16px" }}>
+        <code style={{ marginTop: "16px", marginRight: "4px" }}>
+          {computed}
+        </code>
+        <button onClick={() => setClipboard(computed)} disabled={isInClipboard}>
+          {isInClipboard ? "Copyed!" : "Copy to clipboard"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/rules.tsx
@@ -0,0 +1,9 @@
+const rules = {
+  o: "0",
+  a: "4",
+  e: "3",
+  i: "1",
+  " ": "_",
+};
+
+export default rules;
